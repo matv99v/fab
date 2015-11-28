0049ebaa32a4a6d60f9969a47fabba3bd85ff567
Vladimir Matveev <matv99v@gmail.com>
2015-11-28 22:52:44 +0200

    second update

--- dist/bundle.js
+++ dist/bundle.js
@@ -44,7 +44,83 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	alert("start!");
+
 	__webpack_require__(1);
+
+	var backEl = document.getElementById('back');
+	var info = document.getElementById("info");
+
+
+
+	var code = [  "T11-B11-T21-R11-T31-R11-T21-B11-T11",
+				  "B21-T21-R23-T11-R23-T21-B21",
+				  "T21-R11-T11-T11-T11-R11-T21",
+				  "T31-B11-T11-B11-T31",
+				  "R11-R32-R11-T11-B11-T11-R11-R32-R11",
+				  "T11-T11-B11-T11-B11-T11-T11",
+				  "T41-B11-T11-B11-T11-B11-T41"
+				];
+
+
+
+
+
+	var code  = code.map(function(line){
+		return line.split('-');
+	});
+
+	var parseColors = {
+		T: "tarnsparent",
+		B: "black",
+		R: "red",
+	};
+
+	function writeInfo(squareMeasure){
+		info.innerHTML = "width: " + window.innerWidth + "<br/>";
+		info.innerHTML += "squareMeasure: " + squareMeasure + "<br/>";
+		info.innerHTML += "sM * 13 = " + squareMeasure.slice(0,-2) * 13;
+	}
+
+
+	function recalculateSquares(){
+		var squareMeasure = Math.floor(window.innerWidth / 13) + "px";
+		writeInfo(squareMeasure);
+
+		while (backEl.firstChild) {
+	    	backEl.removeChild(backEl.firstChild);
+		}
+
+
+		code.forEach(function(line){
+			var tableRow = document.createElement("tr");
+			tableRow.className = "row";
+			backEl.appendChild( tableRow );
+
+
+			line.forEach(function(lineItem){
+				var color = parseColors[lineItem.slice(0,1)];
+				var cSpan = lineItem.slice(1,2);
+				var rSpan = lineItem.slice(2,3);
+				console.log( lineItem, color, cSpan, rSpan );
+
+				var tableData = document.createElement("td");
+
+
+				tableData.className = "square " + color;
+				tableData.colSpan = cSpan;
+				tableData.rowSpan = rSpan;
+
+				tableData.style.width = squareMeasure;
+				tableData.style.height = squareMeasure;
+				tableRow.appendChild( tableData );
+			});
+		});
+	}
+
+	recalculateSquares();
+
+	window.addEventListener("resize", recalculateSquares);
 
 
 /***/ },
@@ -82,7 +158,7 @@
 
 
 	// module
-	exports.push([module.id, ".square {\n  width: 7vmax;\n  height: 7vmax;\n  display: inline-block;\n  opacity: 0.5;\n  border: 5px solid tarnsparent; }\n\n.square:hover {\n  background-color: green; }\n\n.red {\n  background-color: red; }\n\n.black {\n  background-color: black; }\n\n.tarnsparent {\n  background-color: none; }\n", ""]);
+	exports.push([module.id, "* {\n  box-sizing: border-box; }\n\nbody {\n  overflow-y: hidden; }\n\n#info {\n  position: fixed;\n  font-size: 25px;\n  color: white;\n  z-index: 1000; }\n\n.square {\n  opacity: 0.5; }\n\n.square:hover {\n  background: green; }\n\n.red {\n  background-color: red; }\n\n.black {\n  background-color: black; }\n\n.tarnsparent {\n  background-color: none; }\n", ""]);
 
 	// exports
 
